2da86e7a5d1470a9aa6995cf3d2d81d3176bbf51
handong <handong@xiaomi.com>
2017-10-27 10:01:43 +0800

    fix ci build

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/env bash -eu
 
-
-FILE="nr_agent_sdk-v0.16.2.0-beta.x86_64.tar.gz"
-rm -f FILE
-wget "http://download.newrelic.com/agent_sdk/nr_agent_sdk-v0.16.2.0-beta.x86_64.tar.gz"
-tar xvf $FILE
-NEWRELIC_BASE=`pwd`/${FILE%.tar.gz}
-export LD_LIBRARY_PATH=$NEWRELIC_BASE/lib
-
 path=("." "./examples")
 for p in `cat submodules`; do
   path=("${path[@]}" "./$p")
 done
 
 cabal update
+cabal install happy alex
 cabal install\
   --force-reinstalls --reorder-goals --only-dependencies --enable-tests\
-  --extra-include-dirs=$NEWRELIC_BASE/include\
-  --extra-lib-dirs=$NEWRELIC_BASE/lib\
   "${path[@]}"
